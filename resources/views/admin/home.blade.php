da6a8eb6552feb12bf63095cc69084ecc3e0d8f2
Silvio Junior <2real121@gmail.com>
2025-09-25 22:21:39 -0300

    salve

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,11 @@
+@extends('conteudo')
+
+@section('conteudo')
+    <h1>Bem-vindo ao Lambida Doce!</h1>
+
+    <p>
+        Explore nossos deliciosos produtos e aproveite ofertas exclusivas.
+    </p>
+
+    <a href="#">Admin</a>
+@endsection
