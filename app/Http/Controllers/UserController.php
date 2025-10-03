567c264991c8642567ce1215dfb23bf9deff116e
Jose Basoa <jose.de.barros0192@gmail.com>
2025-10-02 22:25:36 -0300

    Cadrasto do usuário

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function create()
+    {
+        return view('user.create');
+    }
+
+    public function store(Request $request)
+    {
+        $usuario = new User();
+
+        $usuario->name = $request->input('name');
+        $usuario->email = $request->input('email');
+        $usuario->password = bcrypt($request->input('password'));
+
+        $usuario->save();
+
+        return redirect()->route('admin.home');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,16 @@
 
 use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+Route::redirect('/', '/admin');
 
 Route::controller(AdminController::class)->group(function() {
     Route::get('/admin', 'Home')->name ('admin.home');
 });
+
+Route::get('/cadastrar', [UserController::class, 'create'])
+->name('user.create');
+
+Route::post('/cadastrar', [UserController::class, 'store'])
+->name('user.store');
